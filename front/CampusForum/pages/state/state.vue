d403b59bc37fecb76ed40476b5483c7606b4bc90
墨羽翎玖 <1597867839@qq.com>
2021-12-12 20:21:35 +0800

    导航栏与基础页面

---
+++ front/CampusForum/pages/state/state.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		我发的状态
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
