3430ce06d4c4014959b810b14d316b1f264ae67d
墨羽翎玖 <1597867839@qq.com>
2021-12-12 13:34:50 +0800

    创建前端项目

---
+++ front/CampusForum/prompts.js
@@ -0,0 +1,30 @@
+module.exports = [{
+  type: 'list',
+  name: 'template',
+  message: '请选择 uni-app 模板',
+  choices: [{
+    name: '默认模板',
+    value: 'default'
+  },{
+    name: '默认模板(TypeScript)',
+    value: 'default-ts'
+  }
+  ],
+  default: 'None'
+},
+{
+  when: answers => answers.template === 'custom',
+  type: 'input',
+  name: 'repo',
+  message: '请输入自定义 uni-app 模板地址',
+  filter (input) {
+    return new Promise(function (resolve, reject) {
+      if (input) {
+        resolve(input)
+      } else {
+        reject(new Error('uni-app 模板地址不能为空'))
+      }
+    })
+  }
+}
+]
