47dbad947ef4acce70fb3297cb3148c9f3a91c34
帆 高 <1206203790@qq.com>
2021-12-18 23:36:21 +0800

    register&login

---
+++ back/CampusForum/CampusForum/Models/Code.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusForum.Models
+{
+    public class Code
+    {
+        public int code { get; set; }
+        public string msg { get; set; }
+        public Object data { get; set; }
+
+        public Code()
+        {
+
+        }
+
+        public Code(int code, string msg, Object data = null)
+        {
+            this.code = code;
+            this.msg = msg;
+            this.data = data;
+        }
+    }
+}
