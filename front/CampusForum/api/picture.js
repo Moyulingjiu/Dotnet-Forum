efd81ce10e4db57b20acbcf01cc81683470cc374
facedawn <1637537024@qq.com>
2021-12-22 00:18:20 +0800

    相册功能完成

---
+++ front/CampusForum/api/picture.js
@@ -0,0 +1,18 @@
+import * as config from '../utils/config.js'
+import * as request from './request.js'
+export const modelUrl = `/picture`
+
+// 根据相册id获取其图片列表
+export function getPictureByAlbumId(id,{page, pageSize=20}) {
+	let url = modelUrl + "/selectAll/"+id;
+	let data = {
+		page: page,
+		pageSize:pageSize
+	}
+	return request.service({
+		url: url,
+		data:data,
+		method: 'GET'
+	})
+}
+
