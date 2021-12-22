24579871ac1ad732e53eb7c85e3a1a5f25613ee4
帆 高 <1206203790@qq.com>
2021-12-22 23:25:22 +0800

    sql、修改用户、分页、逆序问题修改

---
+++ back/CampusForum/CampusForum/Models/UserModifiedReq.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusForum.Models
+{
+    public class UserModifiedReq
+    {
+        public string name { set; get; }
+        public string college { set; get; }
+        public string department { get; set; }
+        public int gender { set; get; }
+        public string avater { get; set; }
+        public string description { set; get; }
+        public string birthday { set; get; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string primarySchool { get; set; }
+        public string juniorHighSchool { get; set; }
+        public string highSchool { get; set; }
+        public string university { get; set; }
+        public int admin { get; set; }
+        public HobbyReq hobbyReq { get; set; }
+       
+    }
+}
