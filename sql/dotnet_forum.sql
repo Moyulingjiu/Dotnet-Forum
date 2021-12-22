c252f1128e6ddf3b9150935b59b4aa93d5ac928c
帆 高 <1206203790@qq.com>
2021-12-22 20:46:00 +0800

    sql文件

--- sql/dotnet_forum.sql
+++ sql/dotnet_forum.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 21/12/2021 14:46:50
+ Date: 22/12/2021 16:40:23
 */
 
 SET NAMES utf8mb4;
@@ -23,7 +23,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `album`;
 CREATE TABLE `album`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
   `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '相册名',
   `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '相册描述',
   `cover` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '封面的url',
@@ -39,7 +39,7 @@ CREATE TABLE `album`  (
 DROP TABLE IF EXISTS `album_picture`;
 CREATE TABLE `album_picture`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `album_id` bigint NULL DEFAULT NULL,
+  `album_id` int NULL DEFAULT NULL,
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -53,10 +53,10 @@ CREATE TABLE `album_picture`  (
 DROP TABLE IF EXISTS `banner`;
 CREATE TABLE `banner`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `create_id` bigint NULL DEFAULT NULL,
+  `create_id` int NULL DEFAULT NULL,
   `url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `comment` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `state_id` bigint NULL DEFAULT NULL,
+  `state_id` int NULL DEFAULT NULL,
   `disable` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -69,12 +69,12 @@ CREATE TABLE `banner`  (
 DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `state_id` bigint NULL DEFAULT NULL,
-  `user_id` bigint NULL DEFAULT NULL,
-  `father_id` bigint NULL DEFAULT NULL COMMENT '父评论id，可以没有',
+  `state_id` int NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  `father_id` int NULL DEFAULT NULL COMMENT '父评论id，可以没有',
   `text` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `disable` int NULL DEFAULT NULL,
-  `reply_id` bigint NULL DEFAULT NULL COMMENT '回复评论id',
+  `reply_id` int NULL DEFAULT NULL COMMENT '回复评论id',
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
@@ -86,12 +86,12 @@ CREATE TABLE `comment`  (
 DROP TABLE IF EXISTS `follow`;
 CREATE TABLE `follow`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NULL DEFAULT NULL,
-  `follower_id` bigint NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  `follower_id` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for like_state
@@ -99,8 +99,8 @@ CREATE TABLE `follow`  (
 DROP TABLE IF EXISTS `like_state`;
 CREATE TABLE `like_state`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `state_id` bigint NULL DEFAULT NULL,
-  `user_id` bigint NULL DEFAULT NULL,
+  `state_id` int NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
   `disable` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -128,8 +128,8 @@ CREATE TABLE `role`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `create_id` bigint NULL DEFAULT NULL,
-  `modify_id` bigint NULL DEFAULT NULL,
+  `create_id` int NULL DEFAULT NULL,
+  `modify_id` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
@@ -141,10 +141,10 @@ CREATE TABLE `role`  (
 DROP TABLE IF EXISTS `role_privilege`;
 CREATE TABLE `role_privilege`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `privilege_id` bigint NULL DEFAULT NULL,
-  `role_id` bigint NULL DEFAULT NULL,
-  `create_id` bigint NULL DEFAULT NULL,
-  `modify_id` bigint NULL DEFAULT NULL,
+  `privilege_id` int NULL DEFAULT NULL,
+  `role_id` int NULL DEFAULT NULL,
+  `create_id` int NULL DEFAULT NULL,
+  `modify_id` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
@@ -156,7 +156,7 @@ CREATE TABLE `role_privilege`  (
 DROP TABLE IF EXISTS `state`;
 CREATE TABLE `state`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
   `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `share_state` int NULL DEFAULT NULL COMMENT '0未分享1已分享',
   `disable` int NULL DEFAULT NULL,
@@ -171,7 +171,7 @@ CREATE TABLE `state`  (
 DROP TABLE IF EXISTS `state_text`;
 CREATE TABLE `state_text`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `state_id` bigint NULL DEFAULT NULL,
+  `state_id` int NULL DEFAULT NULL,
   `text` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `disable` int NOT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -189,18 +189,24 @@ CREATE TABLE `user`  (
   `password` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `college` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `department` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `gender` int NULL DEFAULT NULL COMMENT '0 保密，1 男，2 女',
   `avater` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `birthday` date NULL DEFAULT NULL,
+  `birthday` datetime NULL DEFAULT NULL,
   `phone` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `primary_school` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `junior_high_school` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `high_school` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `university` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `admin` int NULL DEFAULT NULL,
   `sign_state` int NULL DEFAULT NULL COMMENT '0 注册待审核，1 正常',
   `disable` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`, `student_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Table structure for user_check
@@ -209,12 +215,29 @@ DROP TABLE IF EXISTS `user_check`;
 CREATE TABLE `user_check`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `state` int NULL DEFAULT NULL,
-  `user_id` bigint NULL DEFAULT NULL,
-  `reviewer_id` bigint NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  `reviewer_id` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for user_hobby
+-- ----------------------------
+DROP TABLE IF EXISTS `user_hobby`;
+CREATE TABLE `user_hobby`  (
+  `id` bigint NOT NULL,
+  `user_id` bigint NULL DEFAULT NULL,
+  `music` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `book` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `movie` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `game` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `comic` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `sport` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `other` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for user_role
@@ -222,10 +245,10 @@ CREATE TABLE `user_check`  (
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`  (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `user_id` bigint NULL DEFAULT NULL,
-  `role_id` bigint NULL DEFAULT NULL,
-  `create_id` bigint NULL DEFAULT NULL,
-  `modify_id` bigint NULL DEFAULT NULL,
+  `user_id` int NULL DEFAULT NULL,
+  `role_id` int NULL DEFAULT NULL,
+  `create_id` int NULL DEFAULT NULL,
+  `modify_id` int NULL DEFAULT NULL,
   `gmt_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `gmt_modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
